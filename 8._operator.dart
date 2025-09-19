bb50564f13a7de20930ae009b1eee41cb2ced59f
arkhamhelga <arkham.helga.taulana>
2025-09-19 10:48:35 +0700

    baru belajar operator

---
+++ 8._operator.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /**
+   * numberOne penulisan 
+   * dengan camel case
+   * number_one penulisan
+   * dengan snack case
+   */
+
+  var numberOne = 10;
+  var numberTwo = 20;
+}
