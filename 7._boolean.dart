86be826642ed3a27173fdd5530481dfd9df415b9
arkhamhelga <arkham.helga.taulana>
2025-09-19 10:30:54 +0700

    Belajar Boolean

---
+++ 7._boolean.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  /**
+   * simbol ! menandakan not atau bukan
+   */
+
+  /**
+   * !i love u = not i love u
+   * boolean hanya ada true dan false
+   */
+  var isTrue = true;
+
+  print(isTrue); //true
+  print(!isTrue); //false
+  print(true != false); //
+}
