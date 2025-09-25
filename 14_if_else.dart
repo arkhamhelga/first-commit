90524597186c4d3d0b8e05512d709558478838de
arkhamhelga <arkham.helga.taulana>
2025-09-25 14:07:20 +0700

    Belajar if else

---
+++ 14_if_else.dart
@@ -0,0 +1,49 @@
+void main(List<String> args) {
+  /**
+   * if else
+   * jika suatu code memiliki kondisi yang terpenuhi
+   * maka di eksekusi dikonsi yang pertama yakni if
+   * bila tidak terpenuhi maka lanjut
+   * ke kondisi selanjutnya yaitu else
+   */
+
+  var nilaiujian = 80;
+  var capek = true;
+  var jambuka = 7; // jam buka
+  var jamtutup = 16; // jam tutup
+  var jamsekarang = 24; //jam sekarang (saat ini)
+
+  /**
+   * nilai E = <=70 Kurang dari sama dengan 70 (E)
+   * Nilai D = 71-75 // 71-75 Nilai (D)
+   * Nilai c = 76-85 // 76-85 Nilai (C)
+   * Nilai B = 86-90 // 86-90 Nilai (B)
+   * Nilai A = 91-100 // 91-100 Nilai (A)
+   */
+
+  if (nilaiujian >= 80) {
+    print('selamat anda lulus ujian');
+  } else {
+    print('anda akan remedial');
+  }
+
+  if (capek != true) {
+    print('silahkan istirahat');
+  } else {
+    print('enggak capek, gas lanjut dicoding');
+  }
+
+  // 7,16
+  if (jamsekarang < jambuka) {
+    print('Toko masih tutup');
+  } else if (jamsekarang == jamsekarang >= 7 && jamtutup <= 16) {
+    //24 == 7 &&
+    //24 == 7 && 16 //lewat jam
+    print('Toko Buka');
+  } else if (jamsekarang >= jamtutup) {
+    //24 >= 16 //lebih dari jam
+    print('Toko sudah tutup');
+  } else {
+    print('Toko tutup permanen');
+  }
+}
